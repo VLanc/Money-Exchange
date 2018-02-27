d72d9b6cab54bcbbcce6cf62e9f014d3d6aeb77d
VLanc <vlancv@gmail.com>
2018-02-27 11:53:52 +0300

    Money exchange task completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+  let H = 0, Q = 0, D = 0, N = 0, P = 0;
+  let remainingCurrency = currency; // we will subtract from this value coins until we reach 0
+  let resCoinsObj = {};
+
+  if(currency > 10000) {
+    resCoinsObj["error"] = "You are rich, my friend! We don't have so much coins for exchange";
+    return resCoinsObj;
+  }
+
+  if(currency <= 0) {
+    return resCoinsObj;
+  }
+
+  while(remainingCurrency != 0) {
+    if(remainingCurrency - 50 >= 0) {
+      H++;
+      remainingCurrency -= 50;
+    } else if(remainingCurrency - 25 >= 0) {
+      Q++;
+      remainingCurrency -= 25;
+    } else if(remainingCurrency - 10 >= 0) {
+      D++;
+      remainingCurrency -= 10;
+    } else if(remainingCurrency - 5 >= 0) {
+      N++;
+      remainingCurrency -= 5;
+    } else if(remainingCurrency - 1 >= 0) {
+      P++;
+      remainingCurrency -= 1;
+    }
+  }
+
+  if(H) resCoinsObj["H"] = H;
+  if(Q) resCoinsObj["Q"] = Q;
+  if(D) resCoinsObj["D"] = D;
+  if(N) resCoinsObj["N"] = N;
+  if(P) resCoinsObj["P"] = P;
+
+  return resCoinsObj;
 }
